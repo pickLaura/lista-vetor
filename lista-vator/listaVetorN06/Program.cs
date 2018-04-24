d866adb06960c330c1de378113c1bdc724a059a7
pickLaura <38699900+pickLaura@users.noreply.github.com>
2018-04-24 17:29:16 -0300

    listaVetor06
    idade de 6 pesoas

---
+++ lista-vator/listaVetorN06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaVetorN06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]idade=new int[9];
+            Console.WriteLine("dijita sua idade");
+            idade[0]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dijita sua idade");
+            idade[1]=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("dijita sua idade");
+             idade [2]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dijita sua idade");
+            idade[3]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dijita sua idade");
+            idade[4]=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("dijita sua idade");
+            idade[5]=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("dijita sua idade");
+            idade[6]=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("dijita sua idade");
+          idade[7]=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("dijita sua idade");
+            idade[8]=Convert.ToInt32(Console.ReadLine());
+
+
+            
+
+        }
+    }
+}
