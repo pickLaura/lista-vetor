107f5310f8048e56d474508834da24aa9bdfc0fc
pickLaura <38699900+pickLaura@users.noreply.github.com>
2018-04-24 17:15:56 -0300

    lista Vetor 05
    peso de 5 pessoas ,media e soma no final

---
+++ lista-vator/listaVetorN05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaVetorN05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media = 0, soma = 0;
+            double[] peso = new double[5];
+            Console.WriteLine("escreva seu peso ");
+            peso[0] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("escreva seu peso ");
+            peso[1] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("escreva seu peso ");
+            peso[2] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("escreva seu peso ");
+            peso[3] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("escreva seu peso ");
+            peso[4] = Convert.ToDouble(Console.ReadLine());
+            soma = peso[0] + peso[1] + peso[2] + peso[3]+peso[4];
+            media = soma / 5;
+            Console.WriteLine("media dos pesos: " + media);
+            Console.WriteLine("soma dos pasos: "+soma);
+
+        }
+    }
+}
