9e65beb140688a17bcdc9b64417d651652a3a5a4
pickLaura <38699900+pickLaura@users.noreply.github.com>
2018-04-24 16:49:59 -0300

    exerciccio 03 lista de vetor
    7 produto,7 presos solisitados para o usuario
    apresentados no final

---
+++ lista-vator/listaVetorN02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaVetorN02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[10];
+            Console.WriteLine("dijite nome de 10 pessoas");
+            nome[0] = Console.ReadLine();
+            nome[1] = Console.ReadLine();
+            nome[2] = Console.ReadLine();
+            nome[3] = Console.ReadLine();
+            nome[4] = Console.ReadLine();
+            nome[5] = Console.ReadLine();
+            nome[6] = Console.ReadLine();
+            nome[7] = Console.ReadLine();
+            nome[8] = Console.ReadLine();
+            nome[9] = Console.ReadLine();
+
+            Console.WriteLine( "1º nome "+nome[0]);
+            Console.WriteLine("2º nome "+nome[1]);
+            Console.WriteLine("3º nome "+nome[2]);
+            Console.WriteLine("4º nome "+nome[3]);
+            Console.WriteLine("5º nome "+nome[4]);
+            Console.WriteLine("6º nome " + nome[5]);
+            Console.WriteLine("7º nome " + nome[6]);
+            Console.WriteLine("8º nome " + nome[7]);
+            Console.WriteLine("9º nome " + nome[8]);
+            Console.WriteLine("10º nome "+nome[9]);
+
+
+        }
+    }
+}
---
+++ lista-vator/listaVetorN03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaVetorN03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] preso = new double[7];
+            string[] produdo = new string[7];
+            Console.WriteLine("digite o preso de 7 produtos: ");
+
+            preso[0] = Convert.ToDouble(Console.ReadLine());
+            preso[1] = Convert.ToDouble(Console.ReadLine());
+            preso[2] = Convert.ToDouble(Console.ReadLine());
+            preso[3] = Convert.ToDouble(Console.ReadLine());
+            preso[4] = Convert.ToDouble(Console.ReadLine());
+            preso[5] = Convert.ToDouble(Console.ReadLine());
+            preso[6] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("dijite 7 produtos");
+
+            produdo[0] = Console.ReadLine();
+            produdo[1] = Console.ReadLine();
+            produdo[2] = Console.ReadLine();
+            produdo[3] = Console.ReadLine();
+            produdo[4] = Console.ReadLine();
+            produdo[5] = Console.ReadLine();
+            produdo[6] = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("1º Produtos e presos: " + preso[0] +","+ produdo[0]);
+            Console.WriteLine("2º Produtos e presos :" + preso[1] +"," +produdo[1]);
+            Console.WriteLine("3º Produtos e presos: " + preso[2] +"," +produdo[2]);
+            Console.WriteLine("4º Produtos e presos: " + preso[3] +"," +produdo[3]);
+            Console.WriteLine("5º Produtos e presos :" + preso[4] +"," +produdo[4]);
+            Console.WriteLine("6º Produtos e presos :" + preso[5] +"," +produdo[5]);
+            Console.WriteLine("7º Produtos e presos: " + preso[6] +"," +produdo[6]);
+            
+        }
+    }
+}
